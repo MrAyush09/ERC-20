921829fa6de9a0a48705ae3ab13b0d10cd76fb0d
Karan Soni <=>
2022-11-16 20:19:24 +0530

    Project setup is done

---
+++ src/main/java/com/kira/Erc20Application.java
@@ -0,0 +1,11 @@
+package com.kira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Erc20Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Erc20Application.class, args);
+    }
+}
